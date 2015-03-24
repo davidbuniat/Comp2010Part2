f77f75a286d62ae66c5d4f9a75ef488ec70ce369
Mateo Inchaurrandieta <mateo.inchaurrandieta.13@ucl.ac.uk>
2015-03-24 16:07:14 +0000

    Fixed imports, added optimizeMethod method

--- src/comp2010/main/ConstantFolder.java
+++ src/comp2010/main/ConstantFolder.java
@@ -4,9 +4,17 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-import org.apache.bcel.classfile.ClassParser;
-import org.apache.bcel.classfile.JavaClass;
-import org.apache.bcel.generic.ClassGen;
+import com.sun.org.apache.bcel.internal.classfile.ClassParser;
+import com.sun.org.apache.bcel.internal.classfile.Code;
+import com.sun.org.apache.bcel.internal.classfile.JavaClass;
+import com.sun.org.apache.bcel.internal.classfile.Method;
+import com.sun.org.apache.bcel.internal.generic.ClassGen;
+import com.sun.org.apache.bcel.internal.generic.ConstantPoolGen;
+import com.sun.org.apache.bcel.internal.generic.ICONST;
+import com.sun.org.apache.bcel.internal.generic.InstructionHandle;
+import com.sun.org.apache.bcel.internal.generic.InstructionList;
+import com.sun.org.apache.bcel.internal.generic.MethodGen;
+import com.sun.org.apache.bcel.internal.generic.TargetLostException;
 
 public class ConstantFolder
 {
@@ -25,6 +33,55 @@ public class ConstantFolder
 		} catch(IOException e){
 			e.printStackTrace();
 		}
+	}
+	
+	private void optimizeMethod(ClassGen cgen, ConstantPoolGen cpgen, Method method)
+	{
+		// Get the Code of the method, which is a collection of bytecode instructions
+		Code methodCode = method.getCode();
+
+		// Now get the actualy bytecode data in byte array, 
+		// and use it to initialise an InstructionList
+		InstructionList instList = new InstructionList(methodCode.getCode());
+
+		// Initialise a method generator with the original method as the baseline	
+		MethodGen methodGen = new MethodGen(method.getAccessFlags(), method.getReturnType(), method.getArgumentTypes(), null, method.getName(), cgen.getClassName(), instList, cpgen);
+
+		// InstructionHandle is a wrapper for actual Instructions
+		for (InstructionHandle handle : instList.getInstructionHandles())
+		{
+			// if the instruction inside is iconst
+			if (handle.getInstruction() instanceof ICONST)
+			{
+				// insert new one with integer 5, and...
+				instList.insert(handle, new ICONST(5));
+				try
+				{
+					// delete the old one
+					instList.delete(handle);
+
+				}
+				catch (TargetLostException e)
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+
+		// setPositions(true) checks whether jump handles 
+		// are all within the current method
+		instList.setPositions(true);
+
+		// set max stack/local
+		methodGen.setMaxStack();
+		methodGen.setMaxLocals();
+
+		// generate the new method with replaced iconst
+		Method newMethod = methodGen.getMethod();
+		// replace the method in the original class
+		cgen.replaceMethod(method, newMethod);
+
 	}
 	
 	public void optimize()
